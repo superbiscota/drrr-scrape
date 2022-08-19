ee0e4601f3004cdffd3a54a58f213d38f053c128
megutamago <31330976+megutamago@users.noreply.github.com>
2022-08-19 22:56:30 +0900

    Add files via upload

---
+++ code.py
@@ -0,0 +1,19 @@
+﻿import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+session = requests.session()
+
+login_info = {
+    "language":"jp-JP",
+    "icon":"setton",
+    "name":"nyaharo",
+    "login":"login",
+    "token":"35cb62b76541bdb25b7c8976a52e97d2"
+}
+
+url_login = "http://drrrkari.com/#"
+res = session.post(url_login, data=login_info)
+res.raise_for_status()
+
+print(res.text)
